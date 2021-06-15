9dbcfababe9205548f3c239c151231f45691c5e5
8n9ght <80909968+8n9ght@users.noreply.github.com>
2021-06-15 19:50:52 +0200

    Design du panier

--- scripts/displayCart.js
+++ scripts/displayCart.js
@@ -2,28 +2,61 @@
 
 let productInCart = JSON.parse(localStorage.getItem('cart'));
 
+
 function displayCart(){
     if(productInCart === null){
-        document.querySelector('.container--cart').innerHTML += '<p>Votre panier est vide</p>';
+        document.querySelector('.container__cart__products').innerHTML += '<p class="container__cart__products--empty">Votre panier est vide</p>';
     }
         else{
             for(let productToBuy of productInCart){
-            document.querySelector('.container--cart').innerHTML += `<div class="card" data-id="${productToBuy.id}">
-                    <div class="card__banner"><img src="${productToBuy.image}" class="card__banner__img"></div>
-                    <div class="card__content">
-                      <h2 class="card__content__title">${productToBuy.name}</h2>
-                      <p class="card__content__price">${productToBuy.price}</p>
-                      <p class="card__content__description">${productToBuy.description}</p>
-                      </div>
-                  </div>`
+            document.querySelector('.container__cart__products').innerHTML += 
+            `<div class="container__cart__row">
+                <div class="row__img">
+                    <img class="row__img__item" src="${productToBuy.image}" alt="">
+                </div>
+                <div class="row__content">
+                    <div class="row__content__title">
+                        <h2 class="row__content_title__item">${productToBuy.name}</h2>
+                    </div>
+                    <div class="row__content__id">
+                        <p class="row__content__id__item">${productToBuy.id}</p>
+                    </div>
+                    <div class="row__content__description">
+                        <p class="row__content__description__item">${productToBuy.description}</p>
+                    </div>
+                    <div class="row__content__price">
+                        <p class="row__content__price__item">${productToBuy.price}</p>
+                    </div>
+                </div>
+            </div>`;
+            let total = 0;
+            let totalProducts = 0;
+for(var i = 0; i < productToBuy.price.lenght; i++){
+    total += productToBuy.price[i];
+    console.log(total);
+}
+for (var j = 0; j < total.length; j++){
+    totalProducts += total[j];
+    console.log(totalProducts);
+}
+
+document.querySelector('.order__total__item').innerHTML = totalProducts + " €";
         }
     }
+   
 }
 
 displayCart();
 
+
+/* Total panier */
+
+
+
 /* Vider Panier */
-document.querySelector('.container--cart').innerHTML += `<div class="cart__reset"><button class="cart__reset__btn">Vider le panier</button></div>`;
+
+document.querySelector('.order__btns').innerHTML += `<div class="cart__reset"><button class="cart__reset__btn">Vider le panier</button></div>
+<div class="cart__confirm"><button class="cart__confirm__btn">Confirmer la commande</button></div>`;
 
 let trash = document.querySelector('.cart__reset__btn');
 
@@ -32,4 +65,8 @@ function emptyCart(){
     window.location.reload(true);
 }
 
-trash.addEventListener('click', () => emptyCart());
+trash.addEventListener('click', emptyCart);
+
+/* Confirmer la commande */
+
+let order = document.querySelector('.cart__confirm__btn');
